3d28645093a7bf431be4a4e2ecfa6a10b34ca84f
AleenaAsif <aleenaasif.444@gmail.com>
2023-07-31 17:39:43 +0500

    created customer class

---
+++ src/main/java/Customer.java
@@ -0,0 +1,15 @@
+package main.java;
+
+public class Customer {
+
+    private String Name;
+    private int Age;
+    private String City;
+    private double PurchaseAmount;
+
+    public enum Gender{
+        Male, Female, Other
+    }
+
+
+}
