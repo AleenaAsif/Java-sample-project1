c0f2ff27b89235585bb6d38f8154eebf39364eba
AleenaAsif <aleenaasif.444@gmail.com>
2023-08-01 19:08:13 +0500

    testing

--- src/main/java/MarketX.java
+++ src/main/java/MarketX.java
@@ -1,47 +1,31 @@
-package main.java;
 
-import main.java.Customer;
+
 
 import java.util.*;
 import java.util.stream.Collectors;
-
 public class MarketX {
 
-    public static void main(String [] args)
-    {
-        //arrayList of objects
-
-        List<Customer> customers=new ArrayList();
-        customers.add(new Customer("Aleena",21,"New York", Customer.Gender.Female, 345678));
-        customers.add(new Customer("Zummer",30,"New York", Customer.Gender.Female, 123445));
-        customers.add(new Customer("Laiba",21,"New York", Customer.Gender.Female, 102130));
-        customers.add(new Customer("Ali",40,"New York", Customer.Gender.Male, 500000));
-        customers.add(new Customer("Ahmed",39,"New York", Customer.Gender.Male, 100000));
-        customers.add(new Customer("Joe",30,"New York", Customer.Gender.Other, 100000));
-
-
-        //filtering on conditions. filtering, mapping, sorting and collecting
-
-        List<String> filterResults=customers.stream()
-                .filter(customer -> customer.getAge() >= 25 && customer.getAge()<=40)
-                .filter(customer->customer.getCity().equals("New York"))
-                .filter(customer->customer.getGender()== Customer.Gender.Male|| customer.getGender()== Customer.Gender.Female)
+    public static List<String> getFilteredCustomerNames(List<Customer> customers) {
+        return customers.stream()
+                .filter(customer -> customer.getAge() >= 25 && customer.getAge() <= 40)
+                .filter(customer -> customer.getCity().equals("New York"))
+                .filter(customer -> customer.getGender() == Customer.Gender.Male || customer.getGender() == Customer.Gender.Female)
                 .map(Customer::getName)
                 .sorted()
                 .collect(Collectors.toList());
+    }
 
+    public static double getTotalPurchaseAmount(List<Customer> customers) {
+        return customers.stream().mapToDouble(Customer::getPurchaseAmount).sum();
+    }
 
-        double TotalPurchaseAmount;
-        TotalPurchaseAmount = customers.stream().mapToDouble(Customer::getPurchaseAmount).sum();
-
-        Customer customerWithHighestPurchase;
-        customerWithHighestPurchase= customers.stream()
+    public static Customer getCustomerWithHighestPurchase(List<Customer> customers) {
+        return customers.stream()
                 .max(Comparator.comparingDouble(Customer::getPurchaseAmount))
                 .orElse(null);
+    }
 
-
-        //using hashmap to calculate average purchase amount for each city
-
+    public static Map<String, Double> getAveragePurchaseByCity(List<Customer> customers) {
         Map<String, Double> totalPurchaseByCity = customers.stream()
                 .collect(Collectors.groupingBy(Customer::getCity, Collectors.summingDouble(Customer::getPurchaseAmount)));
         Map<String, Double> averagePurchaseByCity = new HashMap<>();
@@ -53,15 +37,29 @@ public class MarketX {
             averagePurchaseByCity.put(city, averagePurchase);
         }
 
+        return averagePurchaseByCity;
+    }
 
+    public static void main(String[] args) {
+        List<Customer> customers = new ArrayList();
+        customers.add(new Customer("Aleena", 21, "New York", Customer.Gender.Female, 345678));
+        customers.add(new Customer("Zummer", 30, "New York", Customer.Gender.Female, 123445));
+        customers.add(new Customer("Laiba", 21, "New York", Customer.Gender.Female, 102130));
+        customers.add(new Customer("Ali", 40, "New York", Customer.Gender.Male, 500000));
+        customers.add(new Customer("Ahmed", 39, "New York", Customer.Gender.Male, 100000));
+        customers.add(new Customer("Joe", 30, "New York", Customer.Gender.Other, 100000));
+
+        List<String> filterResults = getFilteredCustomerNames(customers);
+        double totalPurchaseAmount = getTotalPurchaseAmount(customers);
+        Customer customerWithHighestPurchase = getCustomerWithHighestPurchase(customers);
+        Map<String, Double> averagePurchaseByCity = getAveragePurchaseByCity(customers);
 
         System.out.println("Filtered Customer Names in alphabetical order: " + filterResults);
-        System.out.println("Total Purchase Amount: " + TotalPurchaseAmount);
+        System.out.println("Total Purchase Amount: " + totalPurchaseAmount);
         System.out.println("Customers with Highest Purchase: " + customerWithHighestPurchase);
         System.out.println("Average Purchase Amount by City: ");
         for (String city : averagePurchaseByCity.keySet()) {
             System.out.println("City: " + city + ", Average Purchase Amount: " + averagePurchaseByCity.get(city));
         }
-
     }
 }
---
+++ src/test/java/MarketXTest.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.mockito.Mockito;
+public class MarketXTest {
+
+    public void testGetFilteredCustomerNames() {
+        List<Customer> customers = new ArrayList<>();
+
+    }
+
+}
+
